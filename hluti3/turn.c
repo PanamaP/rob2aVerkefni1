2dfa6b915cefa3447a4980fe470636f8d4725c1f
Elfar <elfarsnaer@gmail.com>
2021-01-19 15:04:45 +0000

    oklarad

--- hluti3/turn.c
+++ hluti3/turn.c
@@ -4,8 +4,12 @@
 #pragma config(Motor,  port1,           rightMotor,    tmotorServoContinuousRotation, openLoop, reversed)
 #pragma config(Motor,  port10,           leftMotor,     tmotorServoContinuousRotation, openLoop)
 
+// dist/ummalhjol = rotation
+
+
 int rotation = 563;
 int power = 93;
+int basedeg = 0;
 
 void drive(){
 	wait1Msec(500);
@@ -20,45 +24,23 @@ void drive(){
   }
 
 }
-// 0 = right turn, 1 = left turn
-void turn(int side){
-	wait1Msec(500);
-	SensorValue[rightEncoder] = 0;
-  SensorValue[leftEncoder] = 0;
 
-		if(side == 0){
-			while(abs(SensorValue[leftEncoder]) < (rotation))
-		  {
-		    motor[rightMotor] = -power;
-		    motor[leftMotor] = -power;
-
-		  }
-	}	else if(side == 1){
-		   while(abs(SensorValue[rightEncoder]) < (rotation))
-		  {
-		    motor[rightMotor] = power;
-		    motor[leftMotor] = power;
-
-		  }
-
-		}
+void turn(int deg, int side){
+	while(SensorValue[leftEncoder] < deg*basedeg){
+	motor[leftMotor] = 127*side;
+	motor[rightMotor] = -127*side;
+	}
 	drive();
 }
-
 
 int nrTurns[14] = {0,1,1,0,0,1,0,0,1,0,0,1,1,0};
 
 task main()
 {
 
-  drive();
-  turn(0);
-  drive();
-  turn(1);
-	/*
   for(int i = 0;i<14;i++){
   	int nr = nrTurns[i];
-  	turn(nr);
-	}*/
+  	turn(90, nr);
+	}
 
 }
